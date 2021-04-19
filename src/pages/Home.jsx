8f7c1b5958004ae3ced8a60a9f3365e4479ff1a3
isaacsquires <isaacsquires@me.com>
2021-04-19 11:52:17 +0100

    fix typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ class Home extends Component {
           <div className='row no-gutters p-2 mt-4'>
               <div className='offset-lg-3 col-lg-6 col-md-12 col-sm-12 p-4 mb-4 fill-light text-center'>
                 <p className='home-body'>
-                The Tools for Learning, Design and Research (tldr) group is a multidisciplinary team based in the Dyson school of Design engineering at Imperial College London. With activities spanning online education, generative design and simulation-based characterisation, this team is brought together by a common theme of creating tool making. As such, we strive to produce easy to use, high quality, open source code and publish supporting content that enables others to make use of it.
+                The Tools for Learning, Design and Research (tldr) group is a multidisciplinary team based in the Dyson school of Design engineering at Imperial College London. With activities spanning online education, generative design and simulation-based characterisation, this team is brought together by a common theme of tool making. As such, we strive to produce easy to use, high quality, open source code and publish supporting content that enables others to make use of it.
                 </p>
               </div>
           </div>
