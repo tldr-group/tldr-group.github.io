3e7728d94c4dd709ad44629687c7b346605f22a9
isaacsquires <isaacsquires@me.com>
2023-03-20 11:42:51 +0000

    add cell norm

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,88 +1,154 @@
 import React, { Component } from "react";
-import taufactor from '../assets/tau_example.png'
-import superres from '../assets/super-res-icon.png'
-import slicegan from '../assets/website_slicegan_img.png'
-import tracex from '../assets/TraceX_logo.png'
-import microlib from '../assets/microlib-logo.png'
-import inpaint from '../assets/inpaint.gif'
-import './projects.css'
+import taufactor from "../assets/tau_example.png";
+import superres from "../assets/super-res-icon.png";
+import slicegan from "../assets/website_slicegan_img.png";
+import tracex from "../assets/TraceX_logo.png";
+import microlib from "../assets/microlib-logo.png";
+import cellnorm from "../assets/cellnorm.png";
+import inpaint from "../assets/inpaint.gif";
+import "./projects.css";
 
 class Projects extends Component {
-    render() {
-      return (
-        <div className=''>
-            <div className='row m-4'>
-                <div className='project offset-lg-1 col-lg-5 col-sm-12 col-md-12 text-center'>
-                <div className="project fill-light  m-4">
-                    <img className='project-img m-4' src={taufactor}></img>
-                    <h1 className='project-title m-2'>TauFactor</h1>
-                    <p className='project-body m-4'>TauFactor is an application for calculating tortuosity factors from tomographic data. TauFactor uses CuPy_ which is an implementation of NumPy-compatible multi-dimensional array on CUDA.</p>
-                    <a className='project-link fill-dark p-2' href="https://github.com/tldr-group/taufactor">Find out more</a>
-                    <div class="span mb-4">...</div>
-                </div>
-
-                <div className="project fill-light  m-4">
-                    <img className='project-img m-4 pt-4' src={tracex}></img>
-                    <h1 className='project-title m-2'>TraceX</h1>
-                    <p className='project-body m-4'>TraceX is a MATLAB application designed to consistently allow the fitting of Isotope Exchange Depth Profile data from Secondary Ion Mass Spectrometry measurements. This allows the extraction of materials properties such as the tracer diffusion coefficient and the surface exchange coefficient.</p>
-                    <a className='project-link fill-dark p-2' href="https://github.com/tldr-group/TraceX-2.0">Find out more</a>
-                    <div class="span mb-4">...</div>
-                    </div>
-
-                    <div className="project fill-light  m-4">
-                    <img className='project-img m-4' src={microlib}></img>
-                    <h1 className='project-title m-2'>microlib</h1>
-                    <p className='project-body m-4'>microlib is a searchable collection of 87 3D microstructures of various materials, intended for use in materials research. These were generated from the DoITPoMS micrograph library using our inpainting and SliceGAN tools.</p>
-                    <a className='project-link fill-dark p-2' href="https://microlib.io">Find out more</a>
-                    <div class="span mb-4">...</div>
-                    </div>
-
-                </div>
-
-
-                <div className='project col-lg-5 col-sm-12 col-md-12 text-center'>
-                    <div className="project fill-light  m-4">
-                    <img className='project-img m-4' src={slicegan}></img>
-                    <h1 className='project-title m-2'>SliceGAN</h1>
-                    <p className='project-body m-4'>SliceGAN is a machine learning algorithm used to generate 3D datasets from a single cross sectional 2D slice.</p>
-                    <a className='project-link fill-dark p-2' href="https://github.com/stke9/SliceGAN">Find out more</a>
-                    <div class="span mb-4">...</div>
-                    </div>
-
-                    <div className="project fill-light  m-4">
-                    <img className='project-img m-4' src={superres}></img>
-                    <h1 className='project-title m-2'>Super-Res</h1>
-                    <p className='project-body m-4'>Super resolution technique that takes as input low-res 3D volume and a high-res multi-phase 2D micrograph and outputs a high-res 3D multi-phase volume.</p>
-                    <a className='project-link fill-dark p-2' href="https://github.com/tldr-group/SuperRes">Find out more</a>
-                    <div class="span mb-4">...</div>
-                    </div>
-
-                    <div className="project fill-light  m-4">
-                    <img className='project-img m-4' src={inpaint}></img>
-                    <h1 className='project-title m-2'>Microstructure inpainter</h1>
-                    <p className='project-body m-4'>Microstructure inpainter is a python app for inpainting material science microstructural images using GANs. The app can be run via a command line interface or a graphical interface.</p>
-                    <a className='project-link fill-dark p-2' href="https://github.com/tldr-group/microstructure-inpainter">Find out more</a>
-                    <div class="span mb-4">...</div>
-                    </div>
-
-                    </div>
-
-
-
+  render() {
+    return (
+      <div className="">
+        <div className="row m-4">
+          <div className="project offset-lg-1 col-lg-5 col-sm-12 col-md-12 text-center">
+            <div className="project fill-light  m-4">
+              <img className="project-img m-4" src={taufactor}></img>
+              <h1 className="project-title m-2">TauFactor</h1>
+              <p className="project-body m-4">
+                TauFactor is an application for calculating tortuosity factors
+                from tomographic data. TauFactor uses CuPy_ which is an
+                implementation of NumPy-compatible multi-dimensional array on
+                CUDA.
+              </p>
+              <a
+                className="project-link fill-dark p-2"
+                href="https://github.com/tldr-group/taufactor"
+              >
+                Find out more
+              </a>
+              <div class="span mb-4">...</div>
             </div>
 
-
+            <div className="project fill-light  m-4">
+              <img className="project-img m-4 pt-4" src={tracex}></img>
+              <h1 className="project-title m-2">TraceX</h1>
+              <p className="project-body m-4">
+                TraceX is a MATLAB application designed to consistently allow
+                the fitting of Isotope Exchange Depth Profile data from
+                Secondary Ion Mass Spectrometry measurements. This allows the
+                extraction of materials properties such as the tracer diffusion
+                coefficient and the surface exchange coefficient.
+              </p>
+              <a
+                className="project-link fill-dark p-2"
+                href="https://github.com/tldr-group/TraceX-2.0"
+              >
+                Find out more
+              </a>
+              <div class="span mb-4">...</div>
             </div>
 
+            <div className="project fill-light  m-4">
+              <img className="project-img m-4" src={microlib}></img>
+              <h1 className="project-title m-2">microlib</h1>
+              <p className="project-body m-4">
+                microlib is a searchable collection of 87 3D microstructures of
+                various materials, intended for use in materials research. These
+                were generated from the DoITPoMS micrograph library using our
+                inpainting and SliceGAN tools.
+              </p>
+              <a
+                className="project-link fill-dark p-2"
+                href="https://microlib.io"
+              >
+                To microlib.io
+              </a>
+              <div class="span mb-4">...</div>
+            </div>
 
-            
+            <div className="project fill-light  m-4">
+              <img className="project-img m-4" src={cellnorm}></img>
+              <h1 className="project-title m-2">Cell Normaliser</h1>
+              <p className="project-body m-4">
+                Cell Normaliser is a web application that aims to bridge the gap
+                between researchers and industry by providing an intuitive and
+                user-friendly interface for normalising laboratory battery data.
+                This allows for the direct comparison with commercial cell data.
+                The web app provides a range of different normalisations, and
+                the ability to save parameters. Future work will look to provide
+                persistent identifiers.
+              </p>
+              <a
+                className="project-link fill-dark p-2"
+                href="https://helpful-cactus-2d3c48.netlify.app/"
+              >
+                To Cell Normaliser
+              </a>
+              <div class="span mb-4">...</div>
+            </div>
+          </div>
 
-            
-      )
-    }
+          <div className="project col-lg-5 col-sm-12 col-md-12 text-center">
+            <div className="project fill-light  m-4">
+              <img className="project-img m-4" src={slicegan}></img>
+              <h1 className="project-title m-2">SliceGAN</h1>
+              <p className="project-body m-4">
+                SliceGAN is a machine learning algorithm used to generate 3D
+                datasets from a single cross sectional 2D slice.
+              </p>
+              <a
+                className="project-link fill-dark p-2"
+                href="https://github.com/stke9/SliceGAN"
+              >
+                Find out more
+              </a>
+              <div class="span mb-4">...</div>
+            </div>
+
+            <div className="project fill-light  m-4">
+              <img className="project-img m-4" src={superres}></img>
+              <h1 className="project-title m-2">Super-Res</h1>
+              <p className="project-body m-4">
+                Super resolution technique that takes as input low-res 3D volume
+                and a high-res multi-phase 2D micrograph and outputs a high-res
+                3D multi-phase volume.
+              </p>
+              <a
+                className="project-link fill-dark p-2"
+                href="https://github.com/tldr-group/SuperRes"
+              >
+                Find out more
+              </a>
+              <div class="span mb-4">...</div>
+            </div>
+
+            <div className="project fill-light  m-4">
+              <img className="project-img m-4" src={inpaint}></img>
+              <h1 className="project-title m-2">Microstructure inpainter</h1>
+              <p className="project-body m-4">
+                Microstructure inpainter is a python app for inpainting material
+                science microstructural images using GANs. The app can be run
+                via a command line interface or a graphical interface.
+              </p>
+              <a
+                className="project-link fill-dark p-2"
+                href="https://github.com/tldr-group/microstructure-inpainter"
+              >
+                Find out more
+              </a>
+              <div class="span mb-4">...</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Projects
+export default Projects;
 
 // {/* <div class="row">
 //         <div class="col">
