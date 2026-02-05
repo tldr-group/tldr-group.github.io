dc0e22397b4ba634b886bbdeb4ac855d9dcc4b5d
rmdocherty <rmdocherty2@gmail.com>
2026-02-05 12:53:06 +0000

    added projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,37 @@
-export const Projects = () => {
+import * as z from "zod/mini";
+
+import { Project, ProjectSchema } from "../types";
+import projectJSON from "../../public/content/updates/projects.json";
+
+const ProjectComponent = ({ projectData }: { projectData: Project }) => {
+  const { title, desc, outLinks, imagePath } = projectData;
+
   return (
     <div>
-      <h2>Projects</h2>
-      <p>List of Projects will go here.</p>
+      <div>
+        <img src={imagePath} alt={`${title}`} style={{ width: "300px", height: "300px", objectFit: "scale-down" }} />
+      </div>
+      <h3>{title}</h3>
+      <p>{desc}</p>
+      <div style={{ display: "flex", flex: "row" }}>
+        {outLinks.map((link) => (
+          <a href={link.link} target="_blank" rel="noopener noreferrer" style={{ marginRight: "10px" }}>
+            {link.text}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const Projects = () => {
+  const projects: Project[] = z.array(ProjectSchema).parse(projectJSON);
+
+  return (
+    <div>
+      {projects.map((project) => (
+        <ProjectComponent key={project.title} projectData={project} />
+      ))}
     </div>
   );
 };
