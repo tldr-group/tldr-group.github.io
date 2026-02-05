4f2438f2c1cc1fb9a90eb8553867651f5ad69560
rmdocherty <rmdocherty2@gmail.com>
2026-02-05 11:47:58 +0000

    added video content

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,8 +1,33 @@
-export const Videos = () => {
+import * as z from "zod/mini";
+
+import { Video, VideoSchema } from "../types";
+
+import videoJSON from "../content/updates/videos.json";
+
+const VideoComponent = ({ videoData }: { videoData: Video }) => {
+  const { title, link } = videoData;
+
   return (
     <div>
-      <h2>Videos</h2>
-      <p>List of Videos will go here.</p>
+      <p>{title}</p>
+      <iframe
+        width="320"
+        height="240"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        src={link}
+      />
+    </div>
+  );
+};
+
+export const Videos = () => {
+  const videos: Video[] = z.array(VideoSchema).parse(videoJSON);
+  return (
+    <div>
+      {videos.map((v) => (
+        <VideoComponent videoData={v} />
+      ))}
     </div>
   );
 };
