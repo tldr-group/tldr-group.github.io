2f74d92dc77927a287a3b7de28f6ac6101dc028b
rmdocherty <rmdocherty2@gmail.com>
2026-02-05 11:34:28 +0000

    added comments

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -2,27 +2,56 @@ import * as z from "zod/mini";
 
 import { Publication, PublicationSchema } from "../types";
 
+// this imports the json from `content` such that we can use it as a variable
 import publicationsJSON from "../content/updates/publications.json";
 
+// Render publication as link if link supplied
+const PublicationComponent = ({ publicationData }: { publicationData: Publication }) => {
+  // 'destructuring': take the properties of publicationData & assign them to variables with same name
+  // note these are variables, not states: they don't cause a re-render when changed
+  // (we never change them, so not a problem)
+  const { authors, title, journal, year, doi, link } = publicationData;
+
+  // ternary if in javascript: if variable before ? is true, return LHS, else return RHS of :
+  const doiText = doi ? ", DOI:" + doi : "";
+
+  // <span> is inline text element in HTML; {variable} means render this variable as text in the component
+  const innerContent = (
+    <span>
+      {" "}
+      {authors}, "{title}",{"  "}
+      <i>
+        {journal} ({year})
+      </i>
+      {doiText}
+    </span>
+  );
+
+  return (
+    <div>
+      {link ? (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          {innerContent}
+        </a>
+      ) : (
+        <span>{innerContent}</span>
+      )}
+      <a href={link} target="_blank" rel="noopener noreferrer"></a>
+    </div>
+  );
+};
+
 // a simple unorderd list of publications which links to the paper with minor styling
-// could become a fancier component in the future (i.e, abstract Publication into a component)
-// could also use bibtex for publication content and export it here with a button
 export const Publications = () => {
+  // zod is library for parsing json data according to schema (think type / class in other languages). We parse data
   const publications: Publication[] = z.array(PublicationSchema).parse(publicationsJSON);
 
+  // 'map' takes each element in array and applies function to it, in our case this function returns a <li> element with publication component inside
   return (
     <div>
       <ul>
         {publications.map((pub) => (
-          <li key={pub.doi}>
-            <a href={pub.link} target="_blank" rel="noopener noreferrer">
-              {pub.authors}, "{pub.title}",{" "}
-              <i>
-                {pub.journal} ({pub.year})
-              </i>
-              , DOI: {pub.doi}
-            </a>
-          </li>
+          <li key={pub.doi}>{<PublicationComponent publicationData={pub} />}</li>
         ))}
       </ul>
     </div>
